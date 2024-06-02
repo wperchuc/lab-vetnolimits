43313eff937b6b29ac7f04a13db9809bc1923460
wperchuc <148639427+wperchuc@users.noreply.github.com>
2024-06-02 14:41:54 +0000

    Version 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,84 @@
 import streamlit as st 
 import pandas as pd
+import http.client
+import json
+from io import StringIO
 
-st.balloons()
-st.markdown("# Data Evaluation App")
-
-st.write("We are so glad to see you here. ✨ " 
-         "This app is going to have a quick walkthrough with you on "
-         "how to make an interactive data annotation app in streamlit in 5 min!")
-
-st.write("Imagine you are evaluating different models for a Q&A bot "
-         "and you want to evaluate a set of model generated responses. "
-        "You have collected some user data. "
-         "Here is a sample question and response set.")
-
-data = {
-    "Questions": 
-        ["Who invented the internet?"
-        , "What causes the Northern Lights?"
-        , "Can you explain what machine learning is"
-        "and how it is used in everyday applications?"
-        , "How do penguins fly?"
-    ],           
-    "Answers": 
-        ["The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting" 
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds."
-    ]
-}
-
-df = pd.DataFrame(data)
-
-st.write(df)
-
-st.write("Now I want to evaluate the responses from my model. "
-         "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-         "You will now notice our dataframe is in the editing mode and try to "
-         "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇")
-
-df["Issue"] = [True, True, True, False]
-df['Category'] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config = {
-        "Questions":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Answers":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Issue":st.column_config.CheckboxColumn(
-            "Mark as annotated?",
-            default = False
-        ),
-        "Category":st.column_config.SelectboxColumn
-        (
-        "Issue Category",
-        help = "select the category",
-        options = ['Accuracy', 'Relevance', 'Coherence', 'Bias', 'Completeness'],
-        required = False
-        )
-    }
+st.set_page_config(
+    page_title="VETlab - Interpretacja wyników badań",
+    page_icon=":microscope:",  # add a lab microscope icon as the app icon
+    layout="wide"
 )
 
-st.write("You will notice that we changed our dataframe and added new data. "
-         "Now it is time to visualize what we have annotated!")
+# st.balloons()
+st.image("https://vetnolimits.com/wp-content/uploads/2020/11/vetnolimits_logo__color-m.png")
+st.markdown("# VETnolimitsLAB")
+st.markdown("## Interpretacja wyników badań")
 
 st.divider()
 
-st.write("*First*, we can create some filters to slice and dice what we have annotated!")
+st.markdown("### 🩸 KREW")
 
-col1, col2 = st.columns([1,1])
+st.write("Wybierz parametr i jego stan:")
+
+conn = http.client.HTTPSConnection("app.nocodb.com")
+headers = { 'xc-token': "0At1GzBrfpYclyW568ViqVKwYbaOf10vcFo8hnu7" }
+conn.request("GET", "/api/v2/tables/mf02p7riniazneb/records?offset=0&limit=25&where=&viewId=vwpruy8w3xndb3v6", headers=headers)
+response = conn.getresponse()
+res = response.read()
+jsondata = res.decode("utf-8")
+# s = str(response,"utf-8")
+# data = StringIO(s)
+# df = pd.read_csv(data)
+# st.write(decoded)
+
+parsed_data = json.loads(jsondata)
+
+df = pd.DataFrame(
+    data=parsed_data['list'],
+    columns=['Parametr', 'Obniżony parametr ⬇︎', 'Podwyższony parametr ⬆︎']
+    )
+
+# dfdown = pd.DataFrame(
+#     data=parsed_data['list'],
+#     columns=['Obniżony parametr ⬇︎']
+#     )
+# dfup = pd.DataFrame(
+#     data=parsed_data['list'],
+#     columns=['Podwyższony parametr ⬆︎']
+#     )
+# dfp = pd.DataFrame(
+#     data=parsed_data['list'],
+#     columns=["Parametr"]
+#     )
+
+parametr = df['Parametr'].drop_duplicates()
+down = df['Obniżony parametr ⬇︎'].drop_duplicates()
+up = df['Podwyższony parametr ⬆︎'].drop_duplicates()
+
+wybor = st.selectbox(
+    "Parametr",
+    parametr
+)
+
+col1, col2 = st.columns(2)
+
 with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options = new_df.Issue.unique())
+    st.markdown("#### Obniżony parametr ⬇︎")
+    down_value = df.loc[df['Parametr'] == wybor, 'Obniżony parametr ⬇︎'].values[0]
+    st.write(down_value)
+
 with col2:
-    category_filter = st.selectbox("Choose a category", options  = new_df[new_df["Issue"]==issue_filter].Category.unique())
+    st.markdown("#### Podwyższony parametr ⬆︎")
+    up_value = df.loc[df['Parametr'] == wybor, 'Podwyższony parametr ⬆︎'].values[0]
+    st.write(up_value)
 
-st.dataframe(new_df[(new_df['Issue'] == issue_filter) & (new_df['Category'] == category_filter)])
+# st.dataframe(
+#     data=df,
+#     hide_index=True,
+#     )
 
-st.markdown("")
-st.write("*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`")
 
-issue_cnt = len(new_df[new_df['Issue']==True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
 
-col1, col2 = st.columns([1,1])
-with col1:
-    st.metric("Number of responses",issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
 
-df_plot = new_df[new_df['Category']!=''].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x = 'Category', y = 'count')
-
-st.write("Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:")
 
