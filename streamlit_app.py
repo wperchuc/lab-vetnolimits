9f3988bf033fb3b6a8cae8f233752a169f3e1719
wperchuc <w.perchuc@gmail.com>
2024-10-30 18:26:36 +0100

    Restoring 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.set_page_config (page_title="Vetnolimits Lab", page_icon="🔬" layout="wide")
+st.set_page_config (page_title="Vetnolimits Lab")
 
 st.header("Vetnolimits Lab")
 st.subheader("Twoja pomoc w diagnostyce różnicowej")
